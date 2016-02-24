d7b00bc2400801132c53be5ddf74b64c12ce7d6b
JohannesSchulz-Knappe <j.schulz-knappe@mpp.hertie-school.org>
2016-02-24 12:45:55 +0100

    Added first R source file
    R source to get dataset 1

---
+++ r_source_dataset01.r
@@ -0,0 +1,8 @@
+#################################
+# R source to get first data set
+# Johannes Schulz-Knappe
+# 24 February 2016
+# Hertie School of Governance
+#################################
+
+data()
